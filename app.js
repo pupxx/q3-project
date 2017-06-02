1f9ab7002993d6cca2bf1b2fdc2e01b4ff6d4245
Cheryln <cherylnbarber@gmail.com>
2017-06-02 16:38:22 -0700

    -changed port to 5050. -linked moment and angular moment in html and injected in app.module.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+
+app.use('*', function(req, res, next) {
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')})
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/app.module.js
@@ -0,0 +1,9 @@
+console.log('hojo');
+
+(function(){
+  'use strict'
+  angular.module('app', ['ui.router', 'angularMoment'])
+  .constant('API_BASE_URL', 'http://localhost:5050')
+
+
+})()

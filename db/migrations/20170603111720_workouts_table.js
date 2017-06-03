9250f8a072c79a0fbb751b5f31d4073673c22c16
Cheryln <cherylnbarber@gmail.com>
2017-06-03 14:09:32 -0700

    -added 04 paper_session seeds -added 05 workouts_equipment_join seeds -added 06 workouts seeds -added 07 equipment seeds

--- db/migrations/20170603111720_workouts_table.js
+++ db/migrations/20170603111720_workouts_table.js
@@ -2,7 +2,6 @@ exports.up = function(knex) {
   return knex.schema.createTable('workouts', table => {
     table.increments()
     table.string("title").notNullable()
-    table.integer('dvd_session_id').references('dvd_sessions.id').onDelete('CASCADE')
     table.integer('instructor_id').references('instructors.id').onDelete('CASCADE')
     table.integer('workout_category_id').references('workout_categories.id').onDelete('CASCADE')
   })
---
+++ db/seeds/06_workouts.js
@@ -0,0 +1,32 @@
+exports.seed = function(knex) {
+  return knex('workouts').del()
+    .then(() => {
+      return knex('workouts').insert([{
+		 id: 1,
+        title: 'Insanity',
+        instructor_id: 3,
+        workout_category_id: 4,
+        created_at: new Date(),
+        updated_at: new Date()
+      },{
+		 id: 2,
+        title: 'Rockout Knockout',
+        instructor_id: 1,
+        workout_category_id: 1,
+        created_at: new Date(),
+        updated_at: new Date()
+      },{
+		 id: 3,
+        title: 'P90X',
+        instructor_id: 3,
+        workout_category_id: 0,
+        created_at: new Date(),
+        updated_at: new Date()
+      }]);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('workouts_id_seq', (SELECT MAX(id) FROM workouts));"
+      );
+    });
+};

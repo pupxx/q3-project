d9c511b4e278cba8c372989010517dd4d0cefa90
Cheryln <cherylnbarber@gmail.com>
2017-06-05 14:23:12 -0700

    added notes in the dvdcontroller.js file to work through display options

--- server/controller/dvdcontroller.js
+++ server/controller/dvdcontroller.js
@@ -2,13 +2,16 @@ const dvd = require('../Models/dvdmodel');
 const paper = require('../Models/papermodel')
 
 
-function getAll(req, res){
+function getAll (req, res){
   dvd.getAll().then((dworkouts)=>{
     let dvdworkouts = dworkouts
     paper.getAll().then((pworkouts)=>{
       let paperWorkouts = pworkouts
       console.log(dvdworkouts, paperWorkouts);
-
+      // let result = [dvdworkouts, paperWorkouts]
+      // let result = { dvd_workouts: dvdworkouts, paper_workouts: paperWorkouts }
+      // let result = dvdworkouts.concat(paperWorkouts)
+      res.json(result)
     })
   });
 }

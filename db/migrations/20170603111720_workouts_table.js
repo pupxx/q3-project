756136320c6f51f0b8292aadf3da89b4543f1e3d
Cheryln <cherylnbarber@gmail.com>
2017-06-06 13:16:50 -0700

    added body to workouts table and added lorem info in seed file.

--- db/migrations/20170603111720_workouts_table.js
+++ db/migrations/20170603111720_workouts_table.js
@@ -2,6 +2,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('workouts', table => {
     table.increments()
     table.string("title").notNullable()
+    table.text('body')
     table.integer('instructor_id').references('instructors.id').onDelete('CASCADE')
     table.integer('workout_category_id').references('workout_categories.id').onDelete('CASCADE')
     table.timestamps(true, true);
--- db/seeds/10_workouts.js
+++ db/seeds/10_workouts.js
@@ -4,6 +4,7 @@ exports.seed = function(knex) {
       return knex('workouts').insert([{
 		 id: 1,
         title: 'Insanity',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         instructor_id: 3,
         workout_category_id: 1,
         created_at: new Date(),
@@ -11,6 +12,7 @@ exports.seed = function(knex) {
       },{
 		 id: 2,
         title: 'Rockout Knockout',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         instructor_id: 1,
         workout_category_id: 1,
         created_at: new Date(),
@@ -18,6 +20,7 @@ exports.seed = function(knex) {
       },{
 		 id: 3,
         title: 'P90X - Legs and Back',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         instructor_id: 2,
         workout_category_id: 5,
         created_at: new Date(),
@@ -25,6 +28,7 @@ exports.seed = function(knex) {
       },{
 		 id: 4,
         title: 'P90X - Ab Ripper X',
+        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         instructor_id: 2,
         workout_category_id: 7,
         created_at: new Date(),

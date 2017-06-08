57beeba2b8e497d52266951be26147e50692283f
Kay Krewson <kay.k@comcast.net>
2017-06-08 10:24:46 -0700

    amended workout seed file to have a list of exercises instead of lorem ipsum

--- db/seeds/10_workouts.js
+++ db/seeds/10_workouts.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
       return knex('workouts').insert([{
 		 id: 1,
         title: 'Insanity',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 3,
         workout_category_id: 1,
         created_at: new Date(),
@@ -12,7 +12,7 @@ exports.seed = function(knex) {
       },{
 		 id: 2,
         title: 'Rockout Knockout',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 1,
         created_at: new Date(),
@@ -20,7 +20,7 @@ exports.seed = function(knex) {
       },{
 		 id: 3,
         title: 'P90X - Legs and Back',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 2,
         workout_category_id: 5,
         created_at: new Date(),
@@ -28,7 +28,7 @@ exports.seed = function(knex) {
       },{
 		 id: 4,
         title: 'P90X - Ab Ripper X',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 2,
         workout_category_id: 7,
         created_at: new Date(),
@@ -36,7 +36,7 @@ exports.seed = function(knex) {
       },{
 		 id: 5,
         title: 'Drill Max',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 3,
         created_at: new Date(),
@@ -44,7 +44,7 @@ exports.seed = function(knex) {
       },{
 		 id: 6,
         title: 'Cardio Core Circuit',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Cathe Friedrichís Cardio Core Circuit (51 min)\n Premixes \n Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 7,
         created_at: new Date(),
@@ -52,7 +52,7 @@ exports.seed = function(knex) {
       },{
 		 id: 7,
         title: 'Pilates Power-Ball Sculpt',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 5,
         workout_category_id: 10,
         created_at: new Date(),
@@ -60,7 +60,7 @@ exports.seed = function(knex) {
       },{
 		 id: 8,
         title: 'Ripped with HIIT - HIIT Upper Body Circuit',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 9,
         created_at: new Date(),
@@ -68,7 +68,7 @@ exports.seed = function(knex) {
       },{
 		 id: 10,
         title: 'Extreme Burn',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 4,
         workout_category_id: 3,
         created_at: new Date(),
@@ -76,7 +76,7 @@ exports.seed = function(knex) {
       },{
 		 id: 11,
         title: 'Peak 10 Cardio Strength',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 6,
         workout_category_id: 3,
         created_at: new Date(),
@@ -84,7 +84,7 @@ exports.seed = function(knex) {
       },{
 		 id: 12,
         title: 'Boot Camp',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 3,
         created_at: new Date(),
@@ -92,7 +92,7 @@ exports.seed = function(knex) {
       },{
 		 id: 13,
         title: 'Circuit Blast',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 3,
         created_at: new Date(),
@@ -100,7 +100,7 @@ exports.seed = function(knex) {
       },{
 		 id: 14,
         title: 'HIIT 40-20',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 1,
         created_at: new Date(),
@@ -108,7 +108,7 @@ exports.seed = function(knex) {
       },{
 		 id: 15,
         title: 'Lower Body Blast',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 8,
         created_at: new Date(),
@@ -116,7 +116,7 @@ exports.seed = function(knex) {
       },{
 		 id: 16,
         title: 'STS Disc 5: Meso 1 Week 1: Back & Triceps',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 1,
         workout_category_id: 2,
         created_at: new Date(),
@@ -124,7 +124,7 @@ exports.seed = function(knex) {
       },{
 		 id: 17,
         title: 'Horizontal Conditioning: Boot Camp',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 9,
         workout_category_id: 1,
         created_at: new Date(),
@@ -132,7 +132,7 @@ exports.seed = function(knex) {
       },{
 		 id: 18,
         title: 'Burn and Build',
-        body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        body: 'Warm-up (5 min) \nIntervals (39 min)\nRound 1\nSquat digs (30 sec)\nMountain climbers (30 sec)\nApple pull-downs(30 sec)\nRepeat all three\nCore: knee-ins (2 sets of 8)\nRound 2\nHop forward twice, drop to pushup, jump up to standing, 4 jacks (30 sec)\nSumo squats (30 sec) Lateral slides (30 sec)\nRepeat all three\nCore: Triangle choke-holds (12)\nCool-down/Stretch (7 min)',
         instructor_id: 8,
         workout_category_id: 3,
         created_at: new Date(),

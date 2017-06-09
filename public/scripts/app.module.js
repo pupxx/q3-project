0deae1d293e6909109490656f659f6c0083f9c30
Kay Krewson <kay.k@comcast.net>
2017-06-09 11:55:23 -0700

    had to change the database reference in the module, for deploying to heroku

--- public/scripts/app.module.js
+++ public/scripts/app.module.js
@@ -3,8 +3,8 @@ console.log('module file connected');
 (function(){
   'use strict'
   angular.module('app', ['ui.router', 'angularMoment', 'as.sortable'])
-  // .constant('API_BASE_URL', 'https://workout-blender.herokuapp.com')
-  .constant('API_BASE_URL', 'http://localhost:5050')
+  .constant('API_BASE_URL', 'https://workout-blender.herokuapp.com')
+  // .constant('API_BASE_URL', 'http://localhost:5050')
 
 
 })()
